bcf49b205647385b0af8c7ecf895bab8faf5e0b1
Chris Hjelmfelt <christine.hjelmfelt@gmail.com>
2020-11-10 16:31:41 -0700

    Rifle Fires

---
+++ Assets/Scripts/BadgeMove.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BadgeMove : MonoBehaviour
+{
+    bool bounce = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   float yLoc = 0.17f;
+        float zLoc = 5.39f;
+        if (transform.position.x <= 3 && bounce == false) {
+            transform.position += new Vector3(1 * Time.deltaTime, 0, 0);
+        } else if(transform.position.x >= -3) {
+            transform.position -= new Vector3(1 * Time.deltaTime, 0, 0);    
+            bounce = true;
+        } else {
+            transform.position = new Vector3(-3, yLoc, zLoc);
+            bounce = false;
+        }
+    }
+}
---
+++ Assets/Scripts/GunFire.cs
@@ -0,0 +1,50 @@
+﻿    using UnityEngine;
+    using System.Collections;
+     
+    public class GunFire : MonoBehaviour {
+
+        public float fireRate = 0.5f;        
+        private float nextFire = 0;  
+        AudioSource bang;
+        public Transform startPoint;
+        public Transform endPoint;
+     
+        // Use this for initialization
+        void Start () {
+            
+        }
+       
+        // Update is called once per frame
+        void Update () {
+           
+            // Check if the player has pressed the fire button and if enough time has elapsed since they last fired
+            if (Input.GetButtonDown("Fire1") && Time.time > nextFire) 
+            {
+                // Sound effects  
+                bang = GetComponent<AudioSource>();              
+                bang.Play();
+
+                // Update the time when our player can fire next
+                nextFire = Time.time + fireRate;
+
+                // Declare a raycast hit to store information about what our raycast has hit
+                RaycastHit hit;
+
+                bool RayHit = Physics.Raycast (startPoint.position, endPoint.position, out hit, 100);
+                // Check if our raycast has hit anything
+                if (RayHit)
+                {    
+                    print(RayHit);
+                    // Check if the object we hit has a rigidbody attached
+                    // if (hit.rigidbody != null)
+                    // {
+                    //     print(RayHit);
+                    //     // remove this object
+                    //     //Destroy(hit);
+                    // }
+                }            
+            
+            }
+        }
+    }
+
